afdb941e52623c6684ffa9af1b0f747931d513d1
田中孝治郎 kojiro tanaka <kojiro.tanaka@gmo-media.jp>
2016-06-07 11:05:43 +0900

    hoge

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,26 +1,17 @@
-require 'test_helper'
-
-class StaticPagesControllerTest < ActionController::TestCase
-
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+class StaticPagesController < ApplicationController
+  def home
   end
 
-  test "should get home" do
-    get :home
-    assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+  def help
+    hensu = 1
+    if(hensu == 2)
+      logger.info('uso')
+    end
+
+
   end
 
-  test "should get help" do
-    get :help
-    assert_response :success
-    assert_select "title", "Help | #{@base_title}"
-  end
-
-  test "should get about" do
-    get :about
-    assert_response :success
-    assert_select "title", "About | #{@base_title}"
+  def about
+    logger.info('aaa')
   end
 end
