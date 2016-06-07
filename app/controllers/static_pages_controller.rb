5734346c791b0d1b70c3a730347fe67afe0c6d88
田中孝治郎 kojiro tanaka <kojiro.tanaka@gmo-media.jp>
2016-06-07 10:56:30 +0900

    hoge

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,26 @@
-class StaticPagesController < ApplicationController
-  def home
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
-  def help
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
-  def about
+  test "should get help" do
+    get :help
+    assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,11 +1,11 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
-    get :home
-    assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
-  end
+  # test "should get " do
+  #   get :hom
+  #   assert_response :success
+  #   assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+  # end
 
   test "should get help" do
     get :help
@@ -13,10 +13,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get about" do
-    get :about
-    assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
-  end
+  # test "should get about" do
+  #   get :about
+  #   assert_response :success
+  #   assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  # end
 
 end
